024d745ee2c4dc161e5d11c8a493a2d895495c48
Abhinand Manohar OP <103516348+abhinandop878@users.noreply.github.com>
2022-10-17 12:25:30 +0530

    display name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DisplayNameComponent } from './display-name/display-name.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DisplayNameComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/display-name/display-name.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-display-name',
+  templateUrl: './display-name.component.html',
+  styleUrls: ['./display-name.component.css']
+})
+export class DisplayNameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
